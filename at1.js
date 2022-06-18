fc0f75fa43dd9fd0cd2798ed6ca8603c4e9e8e43
jairomaru <jairomura@hotmail.com>
2022-06-18 12:58:31 -0300

    fist

---
+++ at1.js
@@ -0,0 +1,17 @@
+let peso =50;
+
+let listaQtdpecas=0;
+
+let nomepeca= "radiador";
+
+if (peso >= 100){
+  console.log(' Peso possui requisistos para ser cadastrado');
+}
+
+if(listaQtdPecas >= 10){
+  console.log("Não há mais capacidade na lista : FUll!");
+}
+
+if(nomepeca.length <3){
+  console.log("nome invalido, utilize ao menos 3 caracteres");
+}
